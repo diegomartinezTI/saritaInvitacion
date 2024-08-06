21c0a63788b63ffbd3567b4eed50dcb1de6f8255
Diego Martinez <diego.martinez@corusconsulting.com>
2024-08-06 00:16:15 -0600

    fix

--- src/webpack.config.js
+++ src/webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
-const isProduction = process.env.NODE_ENV === 'production';
+const isProduction = 'production';
 
 module.exports = {
   entry: './src/index.js', // O el archivo de entrada de tu aplicación
